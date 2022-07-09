b696f75c2d90ca0bd6919a3db3540e578da9c6b2
Isaac Alvarez Gil <isaacag.developer@gmail.com>
2022-07-09 15:31:27 +0200

    Features [Cache]: Minor Changes
    Gradle project dependencies and some hilt changes.

--- rickandmorty_cache/src/main/java/com/mango/android/rickandmorty_cache/source/RickAndMortyCacheDataSource.kt
+++ rickandmorty_cache/src/main/java/com/mango/android/rickandmorty_cache/source/RickAndMortyCacheDataSource.kt
@@ -4,7 +4,6 @@ import com.mango.android.rickandmorty_cache.database.RickAndMortyDatabase
 import com.mango.android.rickandmorty_cache.models.CurrentPageDbModel
 import javax.inject.Inject
 
-
 class RickAndMortyCacheDataSource @Inject constructor(private val db: RickAndMortyDatabase) {
 
     suspend fun getCurrentPageForListID(listIDSent: Int) = db.rickAndMortyDao().getCurrentPageForListID(listIDSent)
